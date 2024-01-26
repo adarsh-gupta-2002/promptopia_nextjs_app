71e433a358a6f747dfc5a63f54b2c0718fbe8577
Adarsh Gupta <adarshgupta8452@gmail.com>
2024-01-26 16:58:38 +0530

    First commit

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,51 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (err) {
+      console.log("[create-prompt] ", err);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return (
+    <div>
+      <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+      />
+    </div>
+  );
+};
+
+export default CreatePrompt;
---
+++ app/profile/page.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session, status } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) {
+      fetchPosts();
+    }
+  }, [session]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you wamt to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (err) {
+        console.log("[profile/page.js]", err);
+      }
+    }
+  };
+
+  if (posts.length === 0) {
+    return;
+  }
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page."
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
